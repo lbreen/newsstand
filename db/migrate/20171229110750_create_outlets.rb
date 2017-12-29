df45712a8954c520abdd6e335e014946e2030851
Luke Breen <l.breen1@outlook.com>
2017-12-29 12:31:08 +0000

    Added basic bootstrap grid to homepage

--- db/migrate/20171229110750_create_outlets.rb
+++ db/migrate/20171229110750_create_outlets.rb
@@ -3,7 +3,8 @@ class CreateOutlets < ActiveRecord::Migration[5.1]
     create_table :outlets do |t|
       t.string :name
       t.string :url
-      t.string :css_tag
+      t.string :article_css_tag
+      t.string :category_css_tag
       t.date :last_retrieved
 
       t.timestamps
